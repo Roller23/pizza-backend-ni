b1f18434f79a64ea07bd24e5e1f34dd686ef9632
Wiktor Kania <roller47@wp.pl>
2021-10-09 10:13:54 +0200

    add adding pizza

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const orders = []
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.end('<h1>Pizza backend</h1>')
+  res.write('<h1>Pizza backend</h1>')
+  res.write('<h2>Current orders</h2>')
+  for (const order of orders) {
+    res.write(`<h3>${order.name} - ${order.price}</h3>`)
+  }
+  res.end()
 })
 
 app.post('/api/kitchen', (req, res) => {
-  console.log(req.body)
+  console.log('request', req.body)
   if (req.body.name === undefined) {
     return res.json({error: 'Brak rodzaju pizzy'})
   }
   if (req.body.amount === undefined) {
     return res.json({error: 'Brak ilości pizzy'})
   }
-  res.json({
-    "name": "Capriciosa", 
-    "amount": 3,
-    "price": 3.14
-  })
+  const amount = Number(req.body.amount)
+  if (isNaN(amount)) {
+    return res.json({error: 'Niewłaściwa ilość pizzy'})
+  }
+  const price = Math.floor(Math.random() * 10 * amount)
+  const order = {name: req.body.name, amount, price}
+  orders.push(order)
+  res.json(order)
 })
 
 app.listen(port, () => {
