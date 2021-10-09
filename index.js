084d4c5c7ad865064c3e9c9235ae41f9380fa916
Wiktor Kania <roller47@wp.pl>
2021-10-09 10:01:08 +0200

    add api endpoint

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.end('Hello World!')
+  res.end('<h1>Pizza backend</h1>')
+})
+
+app.post('/api/kitchen', (req, res) => {
+  console.log(req.body)
+  if (req.body.name === undefined) {
+    return res.json({error: 'Brak rodzaju pizzy'})
+  }
+  if (req.body.amount === undefined) {
+    return res.json({error: 'Brak ilości pizzy'})
+  }
+  res.json({
+    "name": "Capriciosa", 
+    "amount": 3,
+    "price": 3.14
+  })
 })
 
 app.listen(port, () => {
